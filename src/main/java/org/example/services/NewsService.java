7a4bd711af4edba207d03abb88fb672a2a661f56
Abayev <240918@astanait.edu.kz>
2025-03-06 20:28:08 +0500

    git init

---
+++ src/main/java/org/example/services/NewsService.java
@@ -0,0 +1,15 @@
+package org.example.services;
+import javax.swing.*;
+
+
+public class NewsService extends JFrame{
+
+    public NewsService(){
+        createGui();
+    }
+    private void createGui(){
+
+
+    }
+
+}
