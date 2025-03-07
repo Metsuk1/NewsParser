647de5a5d81d32024aba2fb0826771baa70338d5
Abayev <240918@astanait.edu.kz>
2025-03-08 00:07:15 +0500

    v0.3

--- src/main/java/org/example/clients/NewsClient.java
+++ src/main/java/org/example/clients/NewsClient.java
@@ -6,14 +6,15 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class NewsClient {
-    private static String API_KEY = "";
-    private static String base_url = "";
+    private static String API_KEY = "2bf40265cfc14bf192c91d3c076b3a5c";
+    private static String base_url = "https://newsapi.org/v2/everything?q=";
 
     public static String getNewsByHashtag(String hashtag) {
+        HttpURLConnection connection = null;
         try{
-            String urlString = base_url  + hashtag + "&api_key=" + API_KEY;
+            String urlString = base_url  + hashtag + "&apiKey=" + API_KEY;
             URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -29,16 +30,10 @@ public class NewsClient {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }finally{
+            if(connection != null){
+                connection.disconnect();
+            }
         }
-
-
-
-
     }
-
-
-
-
-
-
 }
